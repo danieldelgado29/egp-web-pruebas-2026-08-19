70af2ae732a8d49be6992837c52a2532cf5d24d4
Daniel Delgado <danieldelgado29@users.noreply.github.com>
2026-09-01 20:51:15 -0500

    Panel: independizar boton de regreso de Ui24R

--- service-worker-6.36.103.js
+++ service-worker-6.36.103.js
@@ -1,5 +1,5 @@
 "use strict";
-const VERSION = "egm-v6.36.103-queue-dom-integrity-v1";
+const VERSION = "egm-v6.36.103-ui24r-back-independent-v1";
 const STATIC_CACHE = `${VERSION}-static`;
 const RUNTIME_CACHE = `${VERSION}-runtime`;
 const STATIC_ASSETS = [

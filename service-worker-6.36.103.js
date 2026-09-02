4518f8e42d54c2988922a40a5f9dafce85680fb2
Daniel Delgado <danieldelgado29@users.noreply.github.com>
2026-09-01 21:14:51 -0500

    Panel: dar estilo Apple al teclado del buscador

--- service-worker-6.36.103.js
+++ service-worker-6.36.103.js
@@ -1,5 +1,5 @@
 "use strict";
-const VERSION = "egm-v6.36.103-search-keyboard-bw-v1";
+const VERSION = "egm-v6.36.103-search-keyboard-apple-v1";
 const STATIC_CACHE = `${VERSION}-static`;
 const RUNTIME_CACHE = `${VERSION}-runtime`;
 const STATIC_ASSETS = [

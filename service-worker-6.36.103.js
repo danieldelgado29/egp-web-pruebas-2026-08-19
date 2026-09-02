ad25608995603506a5e28e438865ca702cf586a6
Daniel Delgado <danieldelgado29@users.noreply.github.com>
2026-09-01 20:08:23 -0500

    Panel: agarre inmediato del regreso Ui24R en Android

--- service-worker-6.36.103.js
+++ service-worker-6.36.103.js
@@ -1,5 +1,5 @@
 "use strict";
-const VERSION = "egm-v6.36.103-ui24r-touch-directo-v1";
+const VERSION = "egm-v6.36.103-ui24r-grab-inmediato-v1";
 const STATIC_CACHE = `${VERSION}-static`;
 const RUNTIME_CACHE = `${VERSION}-runtime`;
 const STATIC_ASSETS = [

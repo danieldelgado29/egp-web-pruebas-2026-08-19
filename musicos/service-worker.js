80491220b34fd07ec0560a43469d2fbbe1492896
Daniel Delgado <danieldelgado29@users.noreply.github.com>
2026-09-15 16:59:18 -0500

    Musicos: fijar AUX y permisos Phone stereo mono

--- musicos/service-worker.js
+++ musicos/service-worker.js
@@ -1,7 +1,7 @@
 "use strict";
-const CACHE = "egp-musicos-v1.5.8.22-ios-old-phone-r27";
+const CACHE = "egp-musicos-v1.5.8.23-phone-stereo-mono-r28";
 const CORE = [
-  "./", "./index.html", "./style.css?v=1.5.8.17", "./app.js?v=ios-old-phone-20260915-r27",
+  "./", "./index.html", "./style.css?v=1.5.8.17", "./app.js?v=ios-old-phone-20260915-r28",
   "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png",
   "../canciones.json", "../configuracion.json"
 ];

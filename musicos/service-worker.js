dfbb00c1c36fb91c4df95af37e6069e89292dada
Daniel Delgado <danieldelgado29@users.noreply.github.com>
2026-09-15 18:00:57 -0500

    Musicos Android: permitir giro vertical horizontal

--- musicos/service-worker.js
+++ musicos/service-worker.js
@@ -1,5 +1,5 @@
 "use strict";
-const CACHE = "egp-musicos-v1.5.8.24-android-dual-pwa-r29";
+const CACHE = "egp-musicos-v1.5.8.25-android-rotate-r30";
 const CORE = [
   "./", "./index.html", "./style.css?v=1.5.8.17", "./app.js?v=ios-old-phone-20260915-r28",
   "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png",
